72ab2728a3546565387219eefbe3ea92e65dcbb5
Rui Wang <ge45dey@mytum.de>
2023-05-18 20:50:08 +0200

    fix(server): fix cors blocking

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import Test from "./Test";
+import Test from "./Test";
 import RegistrationPage from "./pages/RegistrationPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <Test /> */}
+    <Test />
     <RegistrationPage />
   </React.StrictMode>
 );
