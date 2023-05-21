0668f0c581a7dc353c7144d05ec4bc1abcd658de
Aining-effi <effiaidd@gmail.com>
2023-05-21 23:02:23 +0200

    fix(MessageBoard): change in MassageBoard

--- frontend/src/components/MessageBoard.jsx
+++ frontend/src/components/MessageBoard.jsx
@@ -1,124 +1,114 @@
 import React, { useState } from "react";
 
-export default function MessageBoard(){
-    const [currentContent, setCurrentContent] = useState ("")
-    const [firstRecord, setFirstRecord] = useState("");
-    const [secondRecord, setSecondRecord] = useState("");
-    const [thirdRecord, setThirdRecord] = useState("");
-    const festivals = [
-        "Youth day"
-    ];
-    const Component1=() =>{
-        return <div>
-                    <div className="mb-4">
-                        <div
-                            className="w-full bg-gray-400 ">
-                                Youth day
-                        </div>
-                    </div>
+export default function MessageBoard() {
+  const [currentContent, setCurrentContent] = useState("");
+  const [firstRecord, setFirstRecord] = useState("");
+  const [secondRecord, setSecondRecord] = useState("");
+  const [thirdRecord, setThirdRecord] = useState("");
+  const holidays = ["Youth", "Chrismas", "Valentien", "Spring Festival"];
 
-                    <div className="mb-4">
-                        <div
-                            className="w-full bg-gray-400 ">
-                        </div>
-                    </div>
-
-                    <div className="mb-8">
-                        <div
-                            className="w-full bg-gray-400 ">
-                        </div>
-                    </div>
-                </div>;
-    };
-    const Component2=() =>{
-        return <div>
-                    <div className="mb-4 ">
-                    <input
-                        type="text"
-                        id="firstRecord"
-                        name="firstRecord"
-                        value={firstRecord}
-                        onChange={handleInputChange}
-                        className="w-full bg-gray-400"/>
-                    </div>
-
-                    <div className="mb-4">
-                        <input
-                            type="text"
-                            id="secondRecord"
-                            name="secondRecord"
-                            value={secondRecord}
-                            onChange={handleInputChange}
-                            className="w-full bg-gray-400"/>
-                    </div>
-
-                    <div className="mb-8">
-                        <input
-                            type="text"
-                            id="thirdRecord"
-                            name="thirdRecord"
-                            value={thirdRecord}
-                            onChange={handleInputChange}
-                            className="w-full bg-gray-400 "/>
-                    </div>
-                </div>;
-    };
-    
-    const handleInputChange = (event) => {
-        const { name, value} =event.target;
-
-        switch (name) {
-            case "firstRecord":
-                setFirstRecord(value);
-                break;
-            case "secondRecord":
-                setSecondRecord(value);
-                break;
-            case "thirdRecord":
-                setThirdRecord(value);
-                break;        
-            default:
-                break;
-        }
-    };
-
-    
-    let content= <Component2/>;
-    
-    
-
-    const handleButtonClick = (content) => {
-        setCurrentContent(content);
-    }
-    if(currentContent === 'Content 1'){
-        content=<Component1/>;
-    }else {
-        content=<Component2/>;
-    }
-
+  const Component1 = () => {
     return (
-        <div className="h-600 "> 
-            <div className=" h-300 absolute bottom-20 left-20 w-1/4 flex flex-col items-center justify-center bg-background">
-                
-                <div className="mb-8 mt-8 basis-1">
-                            <button type="button" 
-                            className="  py-1 px-5 rounded-full bg-lightButton mr-4"
-                            onClick={()=>handleButtonClick('Content 1')}>
-                            Festivals
-                            </button>
-                            <button type="button" 
-                            className=" py-1 px-5 rounded-full bg-lightButton"
-                            onClick={()=>handleButtonClick('Content 2')}>
-                            Records
-                            </button>
-                </div>
-                <div>{content}</div>
-
-                
-
-
-        
+      <div>
+        <div className="mb-4">
+          {holidays.map((item, index) => (
+            <div className="w-4/5 mx-auto h-7 bg-gray-200 mb-4" key={index}>
+              {item}
             </div>
+          ))}
         </div>
+      </div>
     );
+  };
+  const Component2 = () => {
+    return (
+      <div>
+        <div className="w-4/5 mx-auto h-7 bg-gray-200 mb-4">
+          <input
+            type="text"
+            id="firstRecord"
+            name="firstRecord"
+            value={firstRecord}
+            onChange={handleInputChange}
+            className="w-full mx-auto h-7 bg-gray-200 mb-4"
+          />
+        </div>
+
+        <div className="w-4/5 mx-auto h-7 bg-gray-200 mb-4">
+          <input
+            type="text"
+            id="secondRecord"
+            name="secondRecord"
+            value={secondRecord}
+            onChange={handleInputChange}
+            className="w-full mx-auto h-7 bg-gray-200 mb-4"
+          />
+        </div>
+
+        <div className="w-4/5 mx-auto h-7 bg-gray-200 mb-4">
+          <input
+            type="text"
+            id="thirdRecord"
+            name="thirdRecord"
+            value={thirdRecord}
+            onChange={handleInputChange}
+            className="w-full mx-auto h-7 bg-gray-200 mb-4"
+          />
+        </div>
+      </div>
+    );
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+
+    switch (name) {
+      case "firstRecord":
+        setFirstRecord(value);
+        break;
+      case "secondRecord":
+        setSecondRecord(value);
+        break;
+      case "thirdRecord":
+        setThirdRecord(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  let content = <Component2 />;
+
+  const handleButtonClick = (content) => {
+    setCurrentContent(content);
+  };
+  if (currentContent === "Content 1") {
+    content = <Component1 />;
+  } else {
+    content = <Component2 />;
+  }
+
+  return (
+    <div className="h-600 ">
+      <div className=" h-1/3 absolute bottom-16 left-24 w-1/4  bg-background">
+        <div className="flex flex-row items-center justify-center mb-8 mt-8 ">
+          <button
+            type="button"
+            className="  py-1 px-5 rounded-full bg-lightButton mr-4"
+            onClick={() => handleButtonClick("Content 1")}
+          >
+            Festivals
+          </button>
+          <button
+            type="button"
+            className=" py-1 px-5 rounded-full bg-lightButton"
+            onClick={() => handleButtonClick("Content 2")}
+          >
+            Records
+          </button>
+        </div>
+        <div className="h-32 overflow-y-auto">{content}</div>
+      </div>
+    </div>
+  );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,15 +2,29 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+// <<<<<<< Updated upstream
     extend: {
       colors: {
+        background: '#FFEFDB',
+      lightButton: '#FEC8D8',
+      normalButton: '#D291BC',
+      darkButton: '#892455',
         themeColor: {
           400: "#810910",
           300: "#A11D2B",
           200: "#C12F47",
           100: "#E14363",
+          
         },
       },
+// =======
+//     extend: {},
+//     colors:{
+//       background: '#FFEFDB',
+//       lightButton: '#FEC8D8',
+//       normalButton: '#D291BC',
+//       darkButton: '#892455'
+// >>>>>>> Stashed changes
     },
   },
   plugins: [],
