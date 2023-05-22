ba626dfac52a4f36143e01e9006bbfd1132a4d2a
Aining-effi <effiaidd@gmail.com>
2023-05-22 16:48:56 +0200

    feat(tailwind): add tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,29 +2,48 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-// <<<<<<< Updated upstream
+    // <<<<<<< Updated upstream
     extend: {
       colors: {
-        background: '#FFEFDB',
-      lightButton: '#FEC8D8',
-      normalButton: '#D291BC',
-      darkButton: '#892455',
+        //yellow
+        background: "#FFEFDB",
+        //light yellow
+        lightBackground: "#FFF4E6",
+
+        //Button
+        lightButton: "#FEC8D8",
+        normalButton: "#D291BC",
+        darkButton: "#892455",
+
+        //PlusButton
+        normalPlusButton: "#DD7E9A",
+        lightPlusButton: "#D2A5C3",
+
+        //premium user/join Premium Button
+        premiumButton: "#5D487F",
+        premiumWords: "#FFD700",
+
+        //tag in homepage
+        tag: "#E9E1FF",
+
+        //new price for premium
+        price: "#F68900",
+
         themeColor: {
           400: "#810910",
           300: "#A11D2B",
           200: "#C12F47",
           100: "#E14363",
-          
         },
       },
-// =======
-//     extend: {},
-//     colors:{
-//       background: '#FFEFDB',
-//       lightButton: '#FEC8D8',
-//       normalButton: '#D291BC',
-//       darkButton: '#892455'
-// >>>>>>> Stashed changes
+      // =======
+      //     extend: {},
+      //     colors:{
+      //       background: '#FFEFDB',
+      //       lightButton: '#FEC8D8',
+      //       normalButton: '#D291BC',
+      //       darkButton: '#892455'
+      // >>>>>>> Stashed changes
     },
   },
   plugins: [],
