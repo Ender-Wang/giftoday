94369e1032fcde45b36346ffe0e0aa4b140d1446
Rui Wang <ge45dey@mytum.de>
2023-05-23 09:52:59 +0200

    refactor(HomePage): remove unexpected lines

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,29 +1,18 @@
 import React from "react";
-import HomePage from "./HomePage";
 import MessageBoard from "../components/MessageBoard";
 
 export default function HomePage() {
+  //Get globalState userID and loggedInDate
   const userID = localStorage.getItem("userID");
   const loggedInDate = localStorage.getItem("loggedInDate");
-
-  const containerStyle = {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "flex-start",
-    height: "100vh",
-  };
 
   return (
     <div>
       <div>HomePage</div>
       {userID
-        ? "User " + userID + " logged in at " + loggedInDate
+        ? "User #" + userID + " logged in at " + loggedInDate
         : "No user logged in"}
-      <div></div>
-      <div style={containerStyle}>
-        <h1 style={titleStyle}>Landing Page</h1>
-        <MessageBoard />
-      </div>
+      <MessageBoard />
     </div>
   );
 }
