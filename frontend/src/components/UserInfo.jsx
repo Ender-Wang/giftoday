0ce4916d5cf9682d9aa651e329bf02bb881ef168
Jingyu Wang <wangjingyu0901@gmail.com>
2023-05-24 15:24:50 +0200

    feat(user info): link to back

--- frontend/src/components/UserInfo.jsx
+++ frontend/src/components/UserInfo.jsx
@@ -1,90 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-class UserInfo extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      email: "",
-      address: "",
-      orders: [],
-    };
-  }
+export default function UserInfo() {
+  const [userInfo, setUserInfo] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  componentDidMount() {
-    this.fetchUserData();
-    this.fetchUserOrders();
-  }
-
-  fetchUserData() {
-    setTimeout(() => {
-      const userData = {
-        name: "John Doe",
-        email: "johndoe@example.com",
-        address: "123 Main St, City, Country",
-      };
-      this.setState({
-        name: userData.name,
-        email: userData.email,
-        address: userData.address,
+  useEffect(() => {
+    // Fetch user information from the backend
+    fetch("http://localhost:4000/users")
+      .then((response) => response.json())
+      .then((data) => {
+        setUserInfo(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
       });
-    }, 100);
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
   }
 
-  fetchUserOrders() {
-    // Simulate asynchronous requests to obtain user order data
-    setTimeout(() => {
-      const userOrders = [
-        { id: 1, product: "Product A", price: 10 },
-        { id: 2, product: "Product B", price: 20 },
-        { id: 3, product: "Product C", price: 15 },
-      ];
-      this.setState({ orders: userOrders });
-    }, 100); // Suppose the request takes 0.1 seconds to return data
+  if (error) {
+    return <p>Error occurred: {error.message}</p>;
   }
 
-  render() {
-    const { name, email, address, orders } = this.state;
-
-    return (
-      <div>
-        <h1>User Information</h1>
-        <p>
-          <strong>Name:</strong> {name}
-        </p>
-        <p>
-          <strong>Email:</strong> {email}
-        </p>
-        <p>
-          <strong>Address:</strong> {address}
-        </p>
-
-        <h2>Orders</h2>
-        {orders.length > 0 ? (
-          <table>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Product</th>
-                <th>Price</th>
-              </tr>
-            </thead>
-            <tbody>
-              {orders.map((order) => (
-                <tr key={order.id}>
-                  <td>{order.id}</td>
-                  <td>{order.product}</td>
-                  <td>${order.price}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        ) : (
-          <p>No orders found.</p>
-        )}
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>User Information</h1>
+      {userInfo ? (
+        <div>
+          {userInfo.map((user) => (
+            <div key={user.id}>
+              <p>Name: {user.name}</p>
+              <p>Email: {user.email}</p>
+              <p>password: {user.password}</p>
+              <p>Address:</p>
+              <ul>
+                <li>Postal Code: {user.address.postalCode}</li>
+                <li>Street: {user.address.street}</li>
+                <li>City: {user.address.city}</li>
+                <li>Country: {user.address.country}</li>
+              </ul>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>No user information available.</p>
+      )}
+    </div>
+  );
 }
-
-export default UserInfo;
