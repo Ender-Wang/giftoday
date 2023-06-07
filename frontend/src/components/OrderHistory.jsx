04650396d0f2e91091f0a8a6b928285904dc5507
DiSophie <986633899@qq.com>
2023-06-07 13:53:53 +0200

    feat(orderHistory): test whether could get the order infromation

--- frontend/src/components/OrderHistory.jsx
+++ frontend/src/components/OrderHistory.jsx
@@ -1,106 +1,142 @@
+// const orders = [
+//   {
+//     number: "WU88191111",
+//     deliveredDate: "July 12, 2021",
+//     deliveredDatetime: "2021-07-12",
+//     total: "$160.00",
+//     products: [
+//       {
+//         id: 1,
+//         name: "Micro Backpack",
+//         amount: 1,
+//         href: "#",
+//         price: "$70.00",
+//         imageSrc:
+//           "https://tailwindui.com/img/ecommerce-images/order-history-page-03-product-01.jpg",
+//         imageAlt:
+//           "Moss green canvas compact backpack with double top zipper, zipper front pouch, and matching carry handle and backpack straps.",
+//       },
+//       // More products...
+//       {
+//         id: 2,
+//         name: "Micro Backpack",
+//         amount: 1,
+//         href: "#",
+//         price: "$70.00",
+//         imageSrc:
+//           "https://tailwindui.com/img/ecommerce-images/order-history-page-03-product-01.jpg",
+//         imageAlt:
+//           "Moss green canvas compact backpack with double top zipper, zipper front pouch, and matching carry handle and backpack straps.",
+//       },
+//     ],
+//   },
+//   {
+//     number: "WU88191111",
+//     href: "#",
+//     invoiceHref: "#",
+//     createdDate: "Jul 6, 2021",
+//     createdDatetime: "2021-07-06",
+//     deliveredDate: "July 12, 2021",
+//     deliveredDatetime: "2021-07-12",
+//     total: "$160.00",
+//     products: [
+//       {
+//         id: 1,
+//         name: "Micro Backpack",
+//         amount: 1,
+//         href: "#",
+//         price: "$70.00",
+//         imageSrc:
+//           "https://tailwindui.com/img/ecommerce-images/order-history-page-03-product-01.jpg",
+//         imageAlt:
+//           "Moss green canvas compact backpack with double top zipper, zipper front pouch, and matching carry handle and backpack straps.",
+//       },
+//       // More products...
+//     ],
+//   },
+
+//   // More orders...
+//   {
+//     number: "WU88191111",
+//     href: "#",
+//     invoiceHref: "#",
+//     createdDate: "Jul 6, 2021",
+//     createdDatetime: "2021-07-06",
+//     deliveredDate: "July 12, 2021",
+//     deliveredDatetime: "2021-07-12",
+//     total: "$160.00",
+//     products: [
+//       {
+//         id: 1,
+//         name: "Micro Backpack",
+//         amount: 1,
+//         href: "#",
+//         price: "$70.00",
+//         imageSrc:
+//           "https://tailwindui.com/img/ecommerce-images/order-history-page-03-product-01.jpg",
+//         imageAlt:
+//           "Moss green canvas compact backpack with double top zipper, zipper front pouch, and matching carry handle and backpack straps.",
+//       },
+//       // More products...
+//     ],
+//   },
+// ];
+
 import { Fragment } from "react";
 import { Menu, Transition } from "@headlessui/react";
 import { EllipsisVerticalIcon } from "@heroicons/react/24/outline";
 import { CheckCircleIcon } from "@heroicons/react/20/solid";
+import React, { useEffect, useState } from "react";
+import { getUserID } from "../states/GlobalState";
 
-const orders = [
-  {
-    number: "WU88191111",
-    href: "#",
-    invoiceHref: "#",
-    createdDate: "Jul 6, 2021",
-    createdDatetime: "2021-07-06",
-    deliveredDate: "July 12, 2021",
-    deliveredDatetime: "2021-07-12",
-    total: "$160.00",
-    products: [
-      {
-        id: 1,
-        name: "Micro Backpack",
-        amount: 1,
-        href: "#",
-        price: "$70.00",
-        imageSrc:
-          "https://tailwindui.com/img/ecommerce-images/order-history-page-03-product-01.jpg",
-        imageAlt:
-          "Moss green canvas compact backpack with double top zipper, zipper front pouch, and matching carry handle and backpack straps.",
-      },
-      // More products...
-      {
-        id: 2,
-        name: "Micro Backpack",
-        amount: 1,
-        href: "#",
-        price: "$70.00",
-        imageSrc:
-          "https://tailwindui.com/img/ecommerce-images/order-history-page-03-product-01.jpg",
-        imageAlt:
-          "Moss green canvas compact backpack with double top zipper, zipper front pouch, and matching carry handle and backpack straps.",
-      },
-    ],
-  },
-  {
-    number: "WU88191111",
-    href: "#",
-    invoiceHref: "#",
-    createdDate: "Jul 6, 2021",
-    createdDatetime: "2021-07-06",
-    deliveredDate: "July 12, 2021",
-    deliveredDatetime: "2021-07-12",
-    total: "$160.00",
-    products: [
-      {
-        id: 1,
-        name: "Micro Backpack",
-        amount: 1,
-        href: "#",
-        price: "$70.00",
-        imageSrc:
-          "https://tailwindui.com/img/ecommerce-images/order-history-page-03-product-01.jpg",
-        imageAlt:
-          "Moss green canvas compact backpack with double top zipper, zipper front pouch, and matching carry handle and backpack straps.",
-      },
-      // More products...
-    ],
-  },
+export default function UserInfo() {
+  const [id] = useState(getUserID);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [postalCode, setPostalCode] = useState("");
+  const [street, setStreet] = useState("");
+  const [city, setCity] = useState("");
+  const [country] = useState("Germany");
+  const [addressM, setAddressM] = useState(null);
 
-  // More orders...
-  {
-    number: "WU88191111",
-    href: "#",
-    invoiceHref: "#",
-    createdDate: "Jul 6, 2021",
-    createdDatetime: "2021-07-06",
-    deliveredDate: "July 12, 2021",
-    deliveredDatetime: "2021-07-12",
-    total: "$160.00",
-    products: [
-      {
-        id: 1,
-        name: "Micro Backpack",
-        amount: 1,
-        href: "#",
-        price: "$70.00",
-        imageSrc:
-          "https://tailwindui.com/img/ecommerce-images/order-history-page-03-product-01.jpg",
-        imageAlt:
-          "Moss green canvas compact backpack with double top zipper, zipper front pouch, and matching carry handle and backpack straps.",
-      },
-      // More products...
-    ],
-  },
-];
+  //get ready to fetch data from backend
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-const handleLogin = (event) => {
-  event.preventDefault();
-  window.location.href = "/login";
-};
+  useEffect(() => {
+    // Fetch user information from the backend
+    fetch(`http://localhost:4000/user/${id}/info`)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data && data.length > 0) {
+          const user = data[0];
+          setName(user.name);
+          setEmail(user.email);
+          setPassword(user.password);
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
+          if (user.address && user.address.length > 0) {
+            setAddressM(user.address);
+          }
+        }
 
-export default function Example() {
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log("Error fetching user info:", error);
+        setError(error);
+        setLoading(false);
+      });
+  }, [id]);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error occurred: {error.message}</p>;
+  }
+
   return (
     // <div class=" h-128 overflow-y-scroll scrollbar-thumb-gray-500 scrollbar-track-gray-200 scrollbar-w-2">
     <div className="bg-white">
@@ -116,43 +152,35 @@ export default function Example() {
         {/* give some blank between the top */}
         <div className="mt-16 ">
           <div className="mx-auto max-w-md ">
-            <div className="mx-auto max-w-md space-y-8  lg:px-0">
-              {orders.map((order) => (
-                <div
-                  key={order.number}
-                  className="border-radius: 30px border-gray-300 bg-white sm:rounded-lg sm:border hover:shadow-md"
-                >
-                  <div className="flex items-center border-b border-gray-300 p-4">
-                    <dl className="grid flex-1 grid-cols-2 gap-x-6 text-sm sm:col-span-3 sm:grid-cols-3 lg:col-span-2">
-                      <div>
-                        <dt className="font-medium text-gray-900">
-                          Order number
-                        </dt>
-                        <dd className="mt-1 text-gray-500">{order.number}</dd>
-                      </div>
+            {addressM !== null && (
+              <div className="mx-auto max-w-md space-y-8  lg:px-0">
+                {addressM.map((address) => (
+                  <div
+                    key={address.id}
+                    className="border-radius: 30px border-gray-300 bg-white sm:rounded-lg sm:border hover:shadow-md"
+                  >
+                    <div className="flex items-center border-b border-gray-300 p-4">
+                      <dl className="grid flex-1 grid-cols-2 gap-x-6 text-sm sm:col-span-3 sm:grid-cols-3 lg:col-span-2">
+                        <div>
+                          <dt className="font-medium text-gray-900">
+                            Order number
+                          </dt>
+                          <dd className="mt-1 text-gray-500">
+                            {address.postalCode}
+                          </dd>
+                        </div>
 
-                      {/* <div className="hidden sm:block">
-                        <dt className="font-medium text-gray-900">
-                          Date placed
-                        </dt>
-                        <dd className="mt-1 text-gray-500">
-                          <time dateTime={order.createdDatetime}>
-                            {order.createdDate}
-                          </time>
-                        </dd>
-                      </div> */}
-
-                      <div>
+                        {/* <div>
                         <dt className="font-medium text-gray-900">
                           Total amount
                         </dt>
                         <dd className="mt-1 font-medium text-gray-900">
                           {order.total}
                         </dd>
-                      </div>
+                      </div> */}
 
-                      {/* Cancel button */}
-                      <div className="flex justify-center">
+                        {/* Cancel button */}
+                        {/* <div className="flex justify-center">
                         <button
                           type="submit"
                           className="bg-themeColor-400 hover:bg-themeColor-200 text-white font-medium py-2 px-4 rounded transition-colors duration-600 ease-in-out"
@@ -160,12 +188,12 @@ export default function Example() {
                         >
                           Cancel
                         </button>
-                      </div>
-                    </dl>
-                  </div>
+                      </div> */}
+                      </dl>
+                    </div>
 
-                  <div class="h-64 overflow-y-scroll scrollbar-thumb-gray-500 scrollbar-track-gray-200 scrollbar-w-2">
                     {/* Products */}
+                    {/* <div class="h-64 overflow-y-scroll scrollbar-thumb-gray-500 scrollbar-track-gray-200 scrollbar-w-2">
 
                     <ul role="list" className="divide-y divide-gray-200">
                       {order.products.map((product) => (
@@ -207,10 +235,12 @@ export default function Example() {
                         </li>
                       ))}
                     </ul>
+
+                  </div> */}
                   </div>
-                </div>
-              ))}
-            </div>
+                ))}
+              </div>
+            )}
           </div>
         </div>
       </div>
