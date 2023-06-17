db3b65b5031e373dfbd903e6ed668b2c2bc0d02c
DiSophie <986633899@qq.com>
2023-06-17 17:36:18 +0200

    feat(OrderSummary): update the reduce, add, delete button

--- frontend/src/components/OrderSummary.jsx
+++ frontend/src/components/OrderSummary.jsx
@@ -13,7 +13,7 @@ import { getUserID } from "../states/GlobalState";
 export default function UserInfo() {
   const [id] = useState(getUserID);
 
-  const [orders, setOrders] = useState(null);
+  const [carts, setCarts] = useState(null);
 
   //get ready to fetch data from backend
   const [loading, setLoading] = useState(true);
@@ -22,12 +22,12 @@ export default function UserInfo() {
   useEffect(() => {
     // Fetch user information from the backend
 
-    fetch(`http://localhost:4000/user/${id}/order`)
+    fetch(`http://localhost:4000/user/${id}/cart`)
       .then((response) => response.json())
       .then((data) => {
         if (data && data.length > 0) {
-          const orders = data;
-          setOrders(orders);
+          const carts = data;
+          setCarts(carts);
 
           // if (order.gift && order.gift.length > 0) {
           //   setGift(order.gift);
@@ -54,7 +54,7 @@ export default function UserInfo() {
   const handleDelete = async (OID) => {
     try {
       const response = await fetch(
-        `http://localhost:4000/user/${id}/order/${OID}`,
+        `http://localhost:4000/user/${id}/cart/${OID}`,
         {
           method: "DELETE",
           headers: {
@@ -64,9 +64,56 @@ export default function UserInfo() {
       );
       if (response.ok) {
         console.log(response);
-        setOrders(orders.filter((order) => order.id !== OID));
+        setCarts(carts.filter((cart) => cart.id !== OID));
       } else {
         console.log("Deleting data failed.");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // 点击添加按钮时触发的函数
+  const handleAdd = async (giftId) => {
+    try {
+      const response = await fetch(
+        `http://localhost:4000/user/${id}/cartAdd/${giftId}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (response.ok) {
+        console.log("Gift added successfully to cart");
+        // 在这里可以根据需要进行其他操作，如更新购物车列表等
+        window.location.reload(); // reload current page
+      } else {
+        console.log("Failed to add gift to cart");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleReduce = async (giftId) => {
+    try {
+      const response = await fetch(
+        `http://localhost:4000/user/${id}/cartReduce/${giftId}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (response.ok) {
+        console.log("Gift added successfully to cart");
+
+        window.location.reload(); // reload current page
+      } else {
+        console.log("Failed to add gift to cart");
       }
     } catch (error) {
       console.log(error);
@@ -90,89 +137,79 @@ export default function UserInfo() {
 
           <div
             className="rounded border border-white p-4"
-            style={{ maxHeight: "84vh", overflowY: "auto" }}
+            style={{ maxHeight: "40vh", overflowY: "auto" }}
           >
             {/* give some blank between the top */}
             <div className="mt-0 ">
               <div className="mx-auto max-w-lg ">
-                {orders !== null && (
+                {carts !== null && (
                   <div className="mx-auto max-w-md space-y-3  lg:px-0">
-                    {orders.map((order) => (
+                    {carts.map((cart) => (
                       <div
-                        key={order.id}
+                        key={cart.id}
                         className="border-radius: 30px border-gray-300 bg-white hover:shadow-md sm:rounded-lg sm:border"
                       >
                         {/* Products */}
                         <div class="scrollbar-thumb-gray-500 scrollbar-track-gray-200 scrollbar-w-2 h-64 overflow-y-scroll">
                           <ul role="list" className="divide-y divide-gray-200">
-                            {order.gift.map((gift) => (
-                              <li key={gift.id} className="p-4 sm:p-6">
-                                <div className="flex items-center sm:items-start">
-                                  <div className="h-20 w-20 flex-shrink-0 overflow-hidden rounded-lg bg-gray-200 sm:h-40 sm:w-40">
-                                    {/* <img
+                            <li key={cart.id} className="p-4 sm:p-6">
+                              <div className="flex items-center sm:items-start">
+                                <div className="h-20 w-20 flex-shrink-0 overflow-hidden rounded-lg bg-gray-200 sm:h-40 sm:w-40">
+                                  {/* <img
                                 src={product.imageSrc}
                                 alt={product.imageAlt}
                                 className="h-full w-full object-cover object-center"
                               /> */}
-                                  </div>
+                                </div>
 
-                                  <div className="ml-6 flex-1 text-sm">
-                                    <div className="font-medium text-gray-900">
-                                      <h5
-                                        className="mt-1 "
-                                        style={{ color: "white" }}
+                                <div className="ml-6 flex-1 text-sm">
+                                  <div className="font-medium text-gray-900">
+                                    <h5
+                                      className="mt-1 "
+                                      style={{ color: "white" }}
+                                    >
+                                      11111111111111111111111111
+                                    </h5>
+                                    <h5 className="mt-1">Name: {cart.name}</h5>
+
+                                    <p className="mt-4">Price: {cart.price}</p>
+
+                                    <p className="mt-4">
+                                      <div
+                                        style={{
+                                          display: "flex",
+                                          alignItems: "center",
+                                        }}
                                       >
-                                        11111111111111111111111111
-                                      </h5>
-                                      <h5 className="mt-1">
-                                        Name: {gift.name}
-                                      </h5>
-
-                                      <p className="mt-4">
-                                        Price: {gift.price}
-                                      </p>
-
-                                      <p className="mt-4">
-                                        <div
-                                          style={{
-                                            display: "flex",
-                                            alignItems: "center",
-                                          }}
-                                        >
-                                          <p> Amount : </p>
-                                          <h5 style={{ color: "white" }}>11</h5>
-                                          <MinusCircleIcon
-                                            className="h-5 w-5"
-                                            aria-hidden="true"
-                                            onClick={() =>
-                                              handleDelete(order.id)
-                                            }
-                                          />
-                                          <span style={{ margin: "0 5px" }}>
-                                            {gift.quantity}{" "}
-                                          </span>
-                                          <PlusCircleIcon
-                                            className="h-5 w-5"
-                                            aria-hidden="true"
-                                            onClick={() =>
-                                              handleDelete(order.id)
-                                            }
-                                          />
-                                        </div>
-                                      </p>
-
-                                      <h5 className="mt-1 flex justify-end">
-                                        <TrashIcon
+                                        <p> Amount : </p>
+                                        <h5 style={{ color: "white" }}>11</h5>
+                                        <MinusCircleIcon
                                           className="h-5 w-5"
                                           aria-hidden="true"
-                                          onClick={() => handleDelete(order.id)}
+                                          onClick={() => handleReduce(cart.id)}
                                         />
-                                      </h5>
-                                    </div>
+                                        <span style={{ margin: "0 5px" }}>
+                                          {cart.quantity}{" "}
+                                        </span>
+                                        <PlusCircleIcon
+                                          className="h-5 w-5"
+                                          aria-hidden="true"
+                                          onClick={() => handleAdd(cart.id)}
+                                        />
+                                      </div>
+                                    </p>
+
+                                    <h5 className="mt-1 flex justify-end">
+                                      <TrashIcon
+                                        className="h-5 w-5"
+                                        aria-hidden="true"
+                                        onClick={() => handleDelete(cart.id)}
+                                      />
+                                    </h5>
                                   </div>
                                 </div>
-                              </li>
-                            ))}
+                              </div>
+                            </li>
                           </ul>
                         </div>
                       </div>
