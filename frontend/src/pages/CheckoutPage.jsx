021c10e7af72ae13f40c1c3a4da9bc47dfd9e23b
Jingyu Wang <wangjingyu0901@gmail.com>
2023-06-26 17:36:57 +0200

    Revert "-"
    This reverts commit 061ac1b2eb00c05480d1ac7f60417fedcf4b9715.

--- frontend/src/pages/CheckoutPage.jsx
+++ frontend/src/pages/CheckoutPage.jsx
@@ -34,17 +34,20 @@ function CheckoutPage() {
       });
   }, [id]);
 
-  const deleteAllCartItems = async () => {
+  const deleteAllCartItems  = async () => {
     try {
-      const response = await fetch(`http://localhost:4000/user/${id}/cart`, {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
+      const response = await fetch(
+        `http://localhost:4000/user/${id}/cart`,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
       if (response.ok) {
-        // Clear the carts state
-        setCarts([]);
+       // Clear the carts state
+       setCarts([]);
       } else {
         console.log("Deleting data failed.");
       }
@@ -86,28 +89,20 @@ function CheckoutPage() {
     setSelectedAddress(address);
   };
   const handleSubmit = async () => {
-    if (!carts || carts.length === 0) {
-      setErrorMessage("You don't have any order😣");
-      setTimeout(() => {
-        setErrorMessage("");
-      }, 1000);
-      return;
-    }
     if (!selectedCard) {
-      setErrorMessage("Please select a card 💳 ");
+      setErrorMessage("Please select a card ");
       setTimeout(() => {
         setErrorMessage("");
       }, 1000);
       return;
     }
     if (!selectedAddress) {
-      setErrorMessage("Please select a address📍");
+      setErrorMessage("Please select a address.");
       setTimeout(() => {
         setErrorMessage("");
       }, 1000);
       return;
     }
-
     try {
       let calculatedTotalPrice = totalPrice;
 
