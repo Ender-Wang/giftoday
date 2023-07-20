bcf1198b634bd23e7ddb3ef9289179224611aa0f
Rui Wang <ge45dey@mytum.de>
2023-07-20 11:35:01 +0200

    fix(userDB): update userDB

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/testUserDB2")
+  .connect("mongodb://127.0.0.1:27017/userDB")
   .then(() => {
     console.log("Connected to DB");
     run();
